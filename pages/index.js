8abdc0c7c01ef9fac94d9d7f7fa7ac92f47137a6
oyanguren <33203921+oyanguren@users.noreply.github.com>
2025-04-12 18:26:40 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Chat Bot UA</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="UA" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Vamos a Comenzar <code>pages/index.js</code>
         </p>
       </main>
 
